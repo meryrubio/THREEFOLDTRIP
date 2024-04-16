923cb037b2f817b160235513e9e3a714d443ac5c
meryrubio <mariarg9d@gmail.com>
2024-04-16 16:53:28 +0200

    CREACION DE CLASES

--- Assets/scripts/Astra.cs
+++ Assets/scripts/Astra.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fades : MonoBehaviour
+public class Astra : Character
 {
     // Start is called before the first frame update
     void Start()
