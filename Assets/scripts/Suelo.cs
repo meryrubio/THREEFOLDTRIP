14b111c0ff2593bb43c17550838119ea3d8a45fc
AnyiRL <ruananyi7@gmail.com>
2024-04-22 00:44:42 +0800

    .

---
+++ Assets/scripts/Suelo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suelo : MonoBehaviour
+{
+    public float speed;
+
+    private void Start()
+    {
+        //transform.Translate(new Vector2(-speed, 0.0f) * Time.deltaTime);
+    }
+}
